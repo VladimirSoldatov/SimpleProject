4cfb203a5acc0c7da36bb966ec7e4134bd06ac7f
Vladimir <cbb110@rambler.ru>
2023-07-13 09:47:32 +0300

    Init

--- SimpleProject/Program.cs
+++ SimpleProject/Program.cs
@@ -1,15 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using static System.Console;
 namespace SimpleProject
 {
-    class Program
+    public class Human
+    {
+        string _firstName;
+        string _lastName;
+        DateTime _birthDate;
+        public Human(string fName, string lName)
+        {
+            _firstName = fName;
+            _lastName = lName;
+        }
+        public Human(string fName, string lName,
+        DateTime date)
+        {
+            _lastName = lName;
+            _birthDate = date;
+        }
+        public void Show()
+        {
+            WriteLine($"\nФамилия: {_lastName}" +
+                $" \nИмя: { _firstName}\nДата " +
+                $"рождения: {_birthDate.ToShortDateString()}");
+        }
+    }
+    public class Employee : Human
+    {
+        double _salary;
+        public Employee(string fName, string lName) :
+        base(fName, lName)
+        { }
+        public Employee(string fName, string lName,
+        double salary)
+        : base(fName, lName)
+        {
+            _salary = salary;
+        }
+        public Employee(string fName, string lName,
+        DateTime date, double salary)
+        : base(fName, lName, date)
+        {
+            _salary = salary;
+        }
+        public void Print()
+        {
+            Show();
+            WriteLine($"Заработная плата: {_salary}$");
+        }
+    }
+    class Program 
     {
         static void Main(string[] args)
         {
+            Employee employee = new Employee("John",
+            "Doe");
+            employee.Print();
+            employee = new Employee("Jim", "Beam", 1253);
+            employee.Print();
+            employee = new Employee("Jack", "Smith",
+            DateTime.Now, 3587.43);
+            employee.Print();
         }
     }
 }
