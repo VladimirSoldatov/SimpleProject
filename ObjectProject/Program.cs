788c781bdca4f02288756de67cbd5042b67ac727
Vladimir <cbb110@rambler.ru>
2023-07-13 10:29:27 +0300

    ObjectProject

---
+++ ObjectProject/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using static System.Console;
+namespace SimpleProject
+{
+    public abstract class Human
+    {
+        string _firstName;
+        string _lastName;
+        DateTime _birthDate;
+        public Human(string fName, string lName,
+        DateTime date)
+        {
+            _firstName = fName;
+            _lastName = lName;
+            _birthDate = date;
+        }
+        public abstract void Think();
+        public override string ToString()
+        {
+            return $"\nФамилия: {_lastName}" +
+                $"\nИмя: { _firstName}" +
+                $"\nДата рождения:{ _birthDate.ToLongDateString()}";
+        }
+    }
+    abstract class Learner : Human
+    {
+        string _institution;
+        public Learner(string fName, string lName,
+        DateTime date, string institution) :
+        base(fName, lName, date)
+        {
+            _institution = institution;
+        }
+        public abstract void Study();
+        public override string ToString()
+        {
+            return base.ToString()
+                + $"\nУчебное заведение: { _institution}.";}
+    }
+    class Student : Learner
+    {
+        string _groupName;
+        public Student(string fName, string lName,
+        DateTime date, string institution,
+        string groupName) : base(fName, lName,
+        date, institution)
+        {
+            _groupName = groupName;
+        }
+        public override void Think()
+        {
+            WriteLine("Я думаю как студент.");
+        }
+        public override void Study()
+        {
+            WriteLine("Я изучаю предметы в институте.");
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nУчусь в { _groupName} группе.";
+        }
+    }
+    class SchoolChild : Learner
+    {
+        string _className;
+        public SchoolChild(string fName,
+        string lName, DateTime date,
+        string institution, string className) :
+        base(fName, lName, date, institution)
+        {
+            _className = className;
+        }
+        public override void Think()
+        {
+            WriteLine("Я думаю как школьник.");
+        }
+        public override void Study()
+        {
+            WriteLine("Я изучаю предметы в школе.");
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nУчусь в {_className} классе.";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Learner[] learners =
+            {
+                new Student("John", "Doe", new DateTime(1990, 6, 12), "IT Step", "15PPS21"),
+                new SchoolChild("Jack", "Smith", new DateTime(2008, 4, 18),"School#154", "1-A")
+            };
+            foreach (Learner item in learners)
+            {
+                WriteLine(item);
+                item.Think();
+                item.Study();
+            }
+        }
+    }
+}
